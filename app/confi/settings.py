9912e73a2e4197ac1f7f11c9d5e983df8e701246
Anibal <Raider>
2020-10-11 22:26:48 +0200

    Video 7

--- app/confi/settings.py
+++ app/confi/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
+
+import confi.db as db
+
 
 from pathlib import Path
 
@@ -70,12 +74,7 @@ WSGI_APPLICATION = 'confi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = db.POSTGRESQL
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
