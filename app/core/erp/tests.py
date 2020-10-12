99194972d2754b4ba9cc8326545cda3b114dffeb
Anibal <Raider>
2020-10-12 14:32:54 +0200

    Video10

--- app/core/erp/tests.py
+++ app/core/erp/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from confi.wsgi import *
 
-# Create your tests here.
+from core.erp.models import Type
+
+# LISTAR
+
+#query = Type.objects.all()
+
+#print(query)
+
+# INSERTAR
+
+#t = Type()
+#t.name = 'Hola'
+#t.save()
+
+# EDITAR
+
+try:
+    t = Type.objects.get(id=1)
+    t.name = 'Hola123'
+    t.save()
+except Exception as e:
+    print(e)
+
+# ELIMINAR
+
+#t= Type.objects.get(pk=1)
+#t.delete()
