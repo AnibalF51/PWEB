494560c1febf23b323684f15878c331f719416b4
Anibal <Raider>
2020-10-12 20:05:18 +0200

    Video17

--- app/confi/settings.py
+++ app/confi/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'confi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
