210c4ee18d875f27eec0be10eb31a5901e090faa
Anibal <Raider>
2020-10-12 18:00:15 +0200

    Video16

--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -1,10 +1,18 @@
 
 from django.shortcuts import render
-
+from core.erp.models import Category, Product
 # Create your views here.
 
 def myfirstview(request):
     data = {
-        'name': 'RAIDER'
+        'name': 'RAIDER',
+        'categories': Category.objects.all()
     }
     return render(request, 'index.html', data)
+
+def mysecondview(request):
+    data = {
+        'name': 'RAIDER',
+        'products': Product.objects.all()
+    }
+    return render(request, 'second.html', data)
