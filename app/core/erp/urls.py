1df674cca33f415a1466072007f2ba59c3842bdc
Miguel <milo612>
2020-10-14 23:56:21 +0200

    Video22-2

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from core.erp.view.category.views import *
-from core.erp.views import CategoryListView
+
 
 app_name ='erp'
 
 urlpatterns = [
  path('category/list/', CategoryListView.as_view(), name='category_list'),
-   # path('dos/', mysecondview, name='vista2')
+   path('category/list2/', category_list, name='category_list2'),
 ]
--- app/core/erp/view/category/views.py
+++ app/core/erp/view/category/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from django.utils.decorators import method_decorator
 from core.erp.models import Category
 
 
 def category_list(request):
     data = {
-        'title': 'Listado de Categorias',
+        'title': 'Listado de Categorías',
         'categories': Category.objects.all()
     }
-    return render(request, 'category/list.html',data)
+    return render(request, 'category/list.html', data)
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Categorías'
+        return context
